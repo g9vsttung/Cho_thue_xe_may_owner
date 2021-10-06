ac4fb674dd538ab783c3c1a52e72d382c94a5cd4
linhtnl <lamlinh20001@gmail.com>
2021-10-07 02:24:48 +0700

    Update view and main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:chothuexemay_owner/view_model/google_signin_in_view_model.dart';
 import 'package:chothuexemay_owner/views/Login/login_view.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +13,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-      title: 'Owner',
-      theme: ThemeData(
-        primaryColor: Color(0xFFFFEBEE),
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: LoginView()
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<GoogleSignInViewModel>(
+            create: (context) => GoogleSignInViewModel())
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Owner',
+          theme: ThemeData(
+            primaryColor: Color(0xFFFFEBEE),
+            scaffoldBackgroundColor: Colors.white,
+          ),
+          home: LoginView()),
     );
   }
 }
-
--- lib/views/Login/components/body.dart
+++ lib/views/Login/components/body.dart
@@ -1,45 +1,62 @@
 import 'package:chothuexemay_owner/constants/constant.dart';
+import 'package:chothuexemay_owner/view_model/google_signin_in_view_model.dart';
 import 'package:chothuexemay_owner/views/Home/home_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-class LoginBody extends StatelessWidget{
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+
+class LoginBody extends StatelessWidget {
   Size size;
+
   LoginBody({required this.size});
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<GoogleSignInViewModel>(context, listen: false);
+    final FirebaseApp =
+        GoogleSignInViewModel.initializeFirebase(context: context);
     return Container(
       child: Padding(
-        padding: EdgeInsets.only(top: size.height*0.55),
+        padding: EdgeInsets.only(top: size.height * 0.55),
         child: Center(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(
-                "Login with",
-                style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold
-                ),
+              ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.green[200],
+                      onPrimary: Colors.black,
+                      minimumSize: Size(size.width * 0.8, 50)),
+                  onPressed: () {
+                    provider.googleLogin(context: context);
+                  },
+                  icon: FaIcon(
+                    FontAwesomeIcons.google,
+                    color: Colors.red[800],
+                  ),
+                  label: Text('Đăng nhập với Google')),
+              SizedBox(
+                height: 10,
               ),
-              Text(
-                "Google account",
-                style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold
-                ),
-              ),
-              SizedBox(height: 10,),
-              IconButton(onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return HomeView();
-                },));
-              },
-                icon: Image.asset(StrConstant.iconPath+"google.png",width: size.width*0.2,), iconSize: size.width*0.2,)
+              // IconButton(
+              //   onPressed: () {
+              //     Navigator.push(context, MaterialPageRoute(
+              //       builder: (context) {
+              //         return HomeView();
+              //       },
+              //     ));
+              //   },
+              //   icon: Image.asset(
+              //     StrConstant.iconPath + "google.png",
+              //     width: size.width * 0.2,
+              //   ),
+              //   iconSize: size.width * 0.2,
+              // )
             ],
           ),
         ),
       ),
     );
   }
-
 }
