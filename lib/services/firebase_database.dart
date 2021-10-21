b9290f648799f6adc362670c9ce3b5842bfbcc15
linhtnl <lamlinh20001@gmail.com>
2021-10-21 15:02:19 +0700

    Update storing location to Firebase

--- lib/services/firebase_database.dart
+++ lib/services/firebase_database.dart
@@ -15,7 +15,6 @@ class FirebaseDatabaseCustom {
   final GeoLocatorCustom _geoLocatorCustom = GeoLocatorCustom();
   String userId = "";
   final String _pathOwnerLocation = 'locations/owners/';
-  final String _pathCustomerLocation = 'locations/customers/';
   final String _latitudeChild = "latitude";
   final String _longitudeChild = "longitude";
 
@@ -27,28 +26,10 @@ class FirebaseDatabaseCustom {
     return snapshot.value != null;
   }
 
-  //Đặt bên customer và getOwnerLocation
-  Future<Map<String, LatLng>> getCustomerLocation() async {
-    Map<String, LatLng> listResult = Map();
-    DataSnapshot snapshot = await database.child(_pathCustomerLocation).once();
-    Map x = snapshot.value;
-    if (snapshot.value != null) {
-      snapshot.value.forEach((k, v) =>
-          {listResult['${k}'] = LatLng(v['latitude'], v['longitude'])});
-    }
-    return listResult;
-  }
-
+  //write user's location to FirebaseRealtime
+  //Called when user logining
   Future<void> storingLocationRealtime() async {
-    Map<String, LatLng> list = await getCustomerLocation();
     final _latlng = await _geoLocatorCustom.determinePosition();
-    String start = '${_latlng.latitude},${_latlng.longitude}';
-    String ends = "";
-    list.forEach((key, value) {
-      ends += '${value.latitude},${value.longitude}|';
-    });
-    distanceCalculator(
-        start, ends.replaceRange(ends.length - 1, ends.length, ''));
     if (await _checkExist()) {
       _updateLocation(_latlng);
     } else {
@@ -76,35 +57,5 @@ class FirebaseDatabaseCustom {
         })
         .then((value) => log("Update store location to FirebaseDB"))
         .catchError((error) => {log(error.toString())});
-  }
-
-  // ignore: slash_for_doc_comments
-  /**Booking flow
-   * 1st: when users login -> store location in FirebaseRealtime
-   * 2st: when customer clicks 'Confirm Booking' -> Send [areaId, motorType]  -> find area by name to get ID 
-   * 3st: BE returns a list of owners have in the area for 'customers system' -> compare distances 
-   *                                                                         -> show notification for owner . Over 1min , skip and go to next owner
-   * 4st: if one owner accept -> return success screen
-   *      else over 3 mins -> customer: please try again
-   */
-  // this function will be called when user click booking
-  void distanceCalculator(String start, String ends) async {
-    //This list store distance from the start point to the destination pointS by meters.
-    List<int> distanceList = [];
-    const String _key = 'MkP5cLaGx6mRgulWqb7dSEkFPlZqLsCDNq1ZUku1';
-
-    Uri url = Uri.parse(
-        'https://rsapi.goong.io/DistanceMatrix?origins=${start}&destinations=${ends}&vehicle=bike&api_key=${_key}');
-
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      final body = jsonDecode(response.body);
-      final result = body['rows'][0]['elements'];
-      result.forEach((r) => {
-            if (r['status'] == 'OK') {distanceList.add(r['distance']['value'])}
-          });
-    } else {
-      throw Exception("Unable to perform request");
-    }
   }
 }
--- lib/services/geolocation_service.dart
+++ lib/services/geolocation_service.dart
@@ -10,9 +10,6 @@ class GeoLocatorCustom {
     // Test if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
       return Future.error('Location services are disabled.');
     }
 
@@ -20,11 +17,6 @@ class GeoLocatorCustom {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
         return Future.error('Location permissions are denied');
       }
     }
@@ -35,16 +27,33 @@ class GeoLocatorCustom {
           'Location permissions are permanently denied, we cannot request permissions.');
     }
 
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-
-    //Get city name
     final postion = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.bestForNavigation);
-    var address = await Geocoder.local.findAddressesFromCoordinates(
-        Coordinates(postion.latitude, postion.longitude));
-    String yourCityName = address.first.adminArea.replaceAll('Thành phố ', '');
 
     return LatLng(postion.latitude, postion.longitude);
+  }
+
+  Future<String> getAddress() async {
+    final _position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.bestForNavigation);
+    String yourAdress = "";
+
+    var address = await Geocoder.local.findAddressesFromCoordinates(
+        Coordinates(_position.latitude, _position.longitude));
+    yourAdress = address.first.addressLine;
+
+    return yourAdress;
+  }
+
+  Future<String> getCityName() async {
+    final _position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.bestForNavigation);
+    String yourCityName = "";
+
+    var address = await Geocoder.local.findAddressesFromCoordinates(
+        Coordinates(_position.latitude, _position.longitude));
+    yourCityName = address.first.adminArea.replaceAll('Thành phố ', '');
+
+    return yourCityName;
   }
 }
